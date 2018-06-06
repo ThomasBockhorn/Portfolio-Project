4a9b06b7f45f11f1de8d51a0a488a591da98b05b
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-06-06 12:10:16 -0500

    Deleted unwanted files

--- Projects/old Professional Web Developer Site/pixDesign/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-gulp.task('default',function(){
-    console.log("Hello world");
-});
-gulp.task('styles', function(){
-    gulp.src('sass/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./css'))
-});
--- js/README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio Site #
-
-This page showcases all that I have learned with front-end web development.
