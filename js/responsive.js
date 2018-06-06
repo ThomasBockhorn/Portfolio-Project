2b16c8df09da5863c6b17812d033842e78bf5f0a
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-06-06 11:45:16 -0500

    portfolio initial setup

--- js/responsive.js
+++ js/responsive.js
@@ -1,7 +1,7 @@
 //This will allow for the menu to show and hide;
-var menu = document.querySelector('.hamburger');
-var main = document.querySelector('main');
-var drawer = document.querySelector('.sidebar');
+const menu = document.querySelector('.hamburger');
+const main = document.querySelector('main');
+const drawer = document.querySelector('.sidebar');
 
 menu.addEventListener('click', function(e) {
     drawer.classList.toggle('open');
@@ -9,4 +9,25 @@ menu.addEventListener('click', function(e) {
 });
 main.addEventListener('click', function() {
     drawer.classList.remove('open');
+});
+
+//The animation form for the contact popup
+$(function ()  {
+    // contact form animations
+    $("#contact").click(function () {
+        $('#contactForm').fadeToggle();
+    });
+   $(document).mouseup(function (element) {
+       const container = $("#contactForm");
+
+      if (!container.is(element.target) // if the target of the click isn't the container...
+           && container.has(element.target).length === 0) // ... nor a descendant of the container
+        {
+          container.fadeOut();
+        }
+    });
+    // When the user clicks on <span> (x), close the modal
+    $("span").click(function(){
+        $("#contactForm").fadeOut();
+    });
 });
